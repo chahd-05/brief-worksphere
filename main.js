01a96f3e45512a94a2fe943586e37d9c0ecd2a70
shahd <mazouzchahd12@gmail.com>
2025-11-20 12:02:05 +0100

    feat: the part logic of modal and card

---
+++ main.js
@@ -0,0 +1,63 @@
+const btn = document.querySelector(".btn-1");
+const modal = document.getElementById("add-modal");
+const background = document.getElementById("bg");
+const cancelBtn = document.getElementById("cancel");
+const addBtn = document.getElementById("add-worker");
+
+btn.addEventListener("click",() => {
+    modal.style.display = "block";
+    background.style.display = "block";
+
+});
+
+cancelBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+    background.style.display = "none";
+});
+
+
+const nameinput = document.getElementById("worker-name");
+const roleinput = document.getElementById("worker-role");
+const emailinput = document.getElementById("worker-email");
+const phoneinput = document.getElementById("worker-phone");
+
+const employeeslist = document.getElementById("employees"); 
+
+addBtn.addEventListener("click", ()=> {
+
+    const name = nameinput.value.trim();
+    const role = roleinput.value.trim();
+    const email = emailinput.value.trim();
+    const phone = phoneinput.value.trim();
+    
+    if (!name || !email || !phone){
+        alert ("please fill all the fields !!");
+    return;
+    }
+
+    const employeeId = document.createElement("div");
+    employeeId.classList.add("employeeId");
+    
+    employeeId.innerHTML = 
+   ` <div class="worker-card">
+    <h4>${name}</h4>
+    <p>${role}</p>
+    <p>${email}</p>
+    <button class="remove">❌</button>
+    </div>`
+
+   employeeslist.appendChild(employeeId);
+
+    modal.style.display = "none";
+    background.style.display = "none";
+
+    nameinput.value = "";
+    emailinput.value = "";
+    phoneinput.value = "";
+
+    const removeBtn = employeeId.querySelector(".remove");
+    removeBtn.addEventListener("click", () => {
+        employeeId.remove();
+    });
+
+});
